352b995fbb0e0a1856ccc2c5193d81c7f0863a54
Bogdan Glinin <twinkl3work@gmail.com>
2022-06-17 21:02:45 +0300

    main page

--- js/feedback.js
+++
@@ -1,3 +0,0 @@
-const userName = document.getElementById("feedback_name");
-const userPhoneNumber = document.getElementById("feedback_number");
-const sendButton = document.getElementById("feedback_button");
