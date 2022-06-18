c59a0fb7c5d25404bb4350bf375d17838449f74e
Bogdan Glinin <twinkl3work@gmail.com>
2022-06-18 19:18:38 +0300

    feedback

--- mail.php
+++ mail.php
@@ -1,31 +1,26 @@
 <?php
 
-    $name = $_POST['user_name'];
-    $phone = $_POST['user_phone'];
+$name = $_POST['user_name'];
+$phone = $_POST['user_phone'];
 
-    use PHPMailer\PHPMailer\PHPMailer;
-    use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-    require 'phpmailer/src/Exception.php';
-    require 'phpmailer/src/PHPMailer.php';
+require 'phpmailer/src/Exception.php';
+require 'phpmailer/src/PHPMailer.php';
 
-    $mail = new PHPMailer(true);
-    $mail->CharSet = 'UTF-8';
-    $mail->setLanguage('ru', 'phpmailer/language/');
-    $mail->isHTML(true);
+$mail = new PHPMailer(true);
+$mail->CharSet = 'UTF-8';
+$mail->setLanguage('ru', 'phpmailer/language/');
+$mail->isHTML(true);
 
-    $mail->setFrom('luxuryrentmsk@gmail.com');
-    $mail->addAddress('luxuryrentmanagermsk@gmail.com');
+$mail->setFrom('luxuryrentmsk@gmail.com');
+$mail->addAddress('luxuryrentmanagermsk@gmail.com');
 
-    $mail->Subject = 'Обратная свзяь';
+$mail->Subject = 'Обратная свзяь';
 
-    $body = '' .$name . ' оставил заявку на звонок, его телефон ' .$phone;
-    $mail->Body = $body;
+$body = '' . $name . ' оставил заявку на звонок, его телефон ' . $phone;
+$mail->Body = $body;
 
-    if(!$mail->send()) {
-        echo 'Error';
-    } else {
-        header('location: index2.html');
-    }
-
-    ?>
+$mail->send();
+?>
